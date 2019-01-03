fa49f3282f638b31360faf1cca3253065cdcd86c
Philip Zhou <philip_zhou@singaporeair.com.sg>
2019-01-03 18:04:06 +0800

    script for toggle proxy

--- proxy-toggle.sh
+++ proxy-toggle.sh
@@ -5,7 +5,9 @@
 if [ "$1" = "on" ] ; then
     npm config set proxy http://philip_zhou:PhZhou@12345@proxy2.sq.com.sg:80
     npm config set https-proxy http://philip_zhou:PhZhou@12345@proxy2.sq.com.sg:80
+    git config http.proxy http://philip_zhou:PhZhou@12345@proxy2.sq.com.sg:80
 else
     npm config rm proxy
     npm config rm https-proxy
+    git config http.proxy
 fi
