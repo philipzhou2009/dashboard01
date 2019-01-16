b06df56fa8a7f8844867bc15769ae9f037061e9f
Philip Zhou <philip_zhou@singaporeair.com.sg>
2019-01-16 17:05:04 +0800

    push switch component

--- hapi-reactjs/src/views/Components/RadioSwitchTypeI.js
+++ hapi-reactjs/src/views/Components/RadioSwitchTypeI.js
@@ -1,9 +1,52 @@
-import React, { Component } from 'react';
 
-class RadioSwitchTypeI extends Component {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  Grid,
+  Button,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  HelpBlock,
+} from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
+
+import './RadioSwitchTypeI.css';
+
+class RadioSwitchTypeI extends React.Component {
+  state = {
+    name: '',
+    showValidationMessage: false,
+    showValidationButton: false,
+  };
+
   render() {
-    return (<div>haha</div>);
+    const {
+      name,
+      showValidationMessage,
+      showValidationButton,
+    } = this.state;
+    return (
+      <Grid>
+        <Button onClick={() => {
+          this.setState(state => ({
+            showValidationButton: false,
+            showValidationMessage: true,
+          }));
+        }}> Validate form </Button>
+        <div className="rs-bg">
+          <span className="rs-bg-l">D</span>
+          <span className="rs-bg-r">AAAAAA</span>
+          <CSSTransition
+            in={showValidationMessage}
+            timeout={1000}
+            classNames="rs"
+            unmountOnExit >
+            <div className="rs-sw"><span className="rs-sw-label">XXXX</span></div>
+          </CSSTransition>
+        </div>
+      </Grid>
+    );
   }
 }
-
 export default RadioSwitchTypeI;
